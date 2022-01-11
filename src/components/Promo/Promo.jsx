d6f14909412ec57e88ba7ca03f9446da05df915d
Vyas Sathya <vyas.sathya@gmail.com>
2022-01-10 16:11:15 -0800

    gas

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -112,7 +112,7 @@ const Promo = ({  }) => {
       // const val = await MyContract.methods.mintPresale( num ).send({
       const val = await MyContract.methods.mintMainsale( num ).send({
         from: myAccount,
-        gasPrice: "121000000000",
+        gasPrice: "181000000000",
         value: requiredAmount,
       });
       console.log(val);
