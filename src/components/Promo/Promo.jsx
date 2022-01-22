42b9bdc9620242549e947d01aeae58880427b07d
Vyas Sathya <vyas.sathya@gmail.com>
2022-01-22 13:58:13 -0800

    d

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -176,7 +176,7 @@ const Promo = ({  }) => {
               <button className='btn btn_outline btn_disabled'>Sold Out</button>
             )}
             {sellStatus === 'paused' && (
-              <button className='btn btn_outline btn_disabled'>Paused</button>
+              <button className='btn btn_outline btn_disabled'>Opens 7PM EST Today</button>
             )}
             {sellStatus === 'connect wallet' && (
               <button onClick={connectWallet} className='btn btn_outline btn_disabled'>Connect Wallet</button>
